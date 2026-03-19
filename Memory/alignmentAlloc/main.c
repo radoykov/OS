fcea72531194c49694ea28d4ac73a8984314dac6
radoykov <alex.radoykov@gmail.com>
2026-03-19 19:35:03 +0200

    Correction on alignment alloc

--- Memory/alignmentAlloc/main.c
+++ Memory/alignmentAlloc/main.c
@@ -3,16 +3,13 @@
 
 void *malloc_aligned(size_t size, size_t alignment)
 {
-    // Заделяме допълнително памет за подравняване + място за оригиналния указател
     void *ptr = malloc(size + alignment + sizeof(void *));
     if (!ptr)
         return NULL;
 
-    // Изчисляваме подравнения адрес след мястото за съхранение на оригиналния ptr
     uintptr_t raw_addr = (uintptr_t)ptr + sizeof(void *);
     void *aligned_ptr = (void *)((raw_addr + alignment - 1) & ~(alignment - 1));
 
-    // Записваме оригиналния адрес точно преди подравнения, за да го ползва free_aligned
     ((void **)aligned_ptr)[-1] = ptr;
 
     return aligned_ptr;
